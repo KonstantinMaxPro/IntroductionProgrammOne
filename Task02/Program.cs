408d5f4a49bff010c9b9ffff764c6e29fa471e5d
Konstantin Mosov <paradyx@outlook.com>
2022-06-28 12:30:45 +0800

    add homework - task 2

---
+++ Task02/Program.cs
@@ -0,0 +1,20 @@
+﻿int a;
+int b;
+int maximum = 0;
+int minimum = 0;
+Console.WriteLine("Введите два числа");
+Console.Write("Первое число: ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Второе число: ");
+b = Convert.ToInt32(Console.ReadLine());
+if(a > b)
+{
+    maximum = a;
+    minimum = b;
+}
+else{
+    maximum = b;
+    minimum = a;
+}
+Console.WriteLine();
+Console.WriteLine($"Большее число: {maximum}\nМеньшее число: {minimum}");
