ee8269a99de0ab8b3a1b9c3bf7f5fb21c5b46482
Konstantin Mosov <paradyx@outlook.com>
2022-06-28 15:35:28 +0800

    add homework task 6

---
+++ Task06/Program.cs
@@ -0,0 +1,15 @@
+﻿int number;
+void Even(int arg1){
+if(arg1 % 2 == 0){
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"Число {arg1}: ЧЁТНОЕ");
+}
+else{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"Число {arg1}: НЕЧЁТНОЕ");
+}
+}
+Console.Write("Введите число: ");
+number = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine();
+Even(number);
