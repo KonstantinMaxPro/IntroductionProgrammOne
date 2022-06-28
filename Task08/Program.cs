31105cdd2a7d9ca74beb41f457ba5b633c316584
Konstantin Mosov <paradyx@outlook.com>
2022-06-28 16:18:31 +0800

    add homework taak 8

---
+++ Task08/Program.cs
@@ -0,0 +1,21 @@
+﻿
+void EvenList(int arg1){
+    int count = 1;
+    int result = 0;
+    while(count  <= arg1){
+        if(count  % 2 == 0){
+            result = count;
+            if(count == arg1 || count + 1 == arg1){
+                Console.Write(result);
+            }
+            else{
+                Console.Write($"{result},");
+            }
+        }
+        count ++;
+    }
+}
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine();
+EvenList(number);
