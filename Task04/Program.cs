3921c92c12dd994de7fcc70833de8a9661a41058
Konstantin Mosov <paradyx@outlook.com>
2022-06-28 15:15:09 +0800

    add homework task 4

---
+++ Task04/Program.cs
@@ -0,0 +1,26 @@
+﻿int numOne;
+int numTwo;
+int numThree;
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result){
+        result = arg2;
+    }
+    if(arg3 > result){
+        result = arg3;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите три числа");
+Console.Write("Первое число: ");
+numOne = Convert.ToInt32(Console.ReadLine());
+Console.Write("Второе число: ");
+numTwo = Convert.ToInt32(Console.ReadLine());
+Console.Write("Третье число: ");
+numThree = Convert.ToInt32(Console.ReadLine());
+int maximum = Max(numOne, numTwo, numThree);
+Console.WriteLine();
+Console.WriteLine($"Максимальное число: {maximum}");
