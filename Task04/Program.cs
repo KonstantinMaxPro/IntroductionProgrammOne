ee03a30aaabb34cc290a1577a7220749205beec1
Konstantin Mosov <paradyx@outlook.com>
2022-07-05 00:35:49 +0800

    modify homework Task 04 (оформил)

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -1,26 +1,60 @@
-﻿int numOne;
-int numTwo;
-int numThree;
+﻿int numberOne;
+int numberTwo;
+int numberThree;
 
-int Max(int arg1, int arg2, int arg3)
+int MaxNumber(int arg1, int arg2, int arg3)//функция определения максимального числа
 {
     int result = arg1;
-    if(arg2 > result){
+    if(arg2 > result)
+    {
         result = arg2;
     }
-    if(arg3 > result){
+    if(arg3 > result)
+    {
         result = arg3;
     }
     return result;
 }
+//ВНЕШНИЙ ВИД ПРОГРАММЫ КОНСОЛИ
+Console.Clear();//Очищаем консоль
+/*ЗАГОЛОВОК (ДЛЯ КРАСОТЫ)*/
+Console.WriteLine("==============================");
+Console.WriteLine("МАКСИМАЛЬНОЕ ИЗ ТРЕХ ЧИСЕЛ");//Название
+Console.WriteLine("==============================\n");
+/*КОНЕЦ ЗАГОЛОВКА*/
 
-Console.WriteLine("Введите три числа");
-Console.Write("Первое число: ");
-numOne = Convert.ToInt32(Console.ReadLine());
-Console.Write("Второе число: ");
-numTwo = Convert.ToInt32(Console.ReadLine());
-Console.Write("Третье число: ");
-numThree = Convert.ToInt32(Console.ReadLine());
-int maximum = Max(numOne, numTwo, numThree);
-Console.WriteLine();
+
+Console.Write("Введите первое число: ");
+/*Проверка ввода первого числа на int*/
+while (!int.TryParse(Console.ReadLine(), out numberOne))
+{
+    Console.ForegroundColor = ConsoleColor.Red;//ошибка красного цвета
+    Console.WriteLine("ОШИБКА! ВЫ ВВЕЛИ НЕ ЦЕЛОЧИСЛЕННОЕ ЗНАЧЕНИЕ\n");   
+    Console.ResetColor();
+    Console.Write("Введите первое число: ");
+}
+Console.Write("Введите второе число: ");
+/*Проверка ввода первого числа на int*/
+while (!int.TryParse(Console.ReadLine(), out numberTwo))
+{
+    Console.ForegroundColor = ConsoleColor.Red;//ошибка красного цвета
+    Console.WriteLine("ОШИБКА! ВЫ ВВЕЛИ НЕ ЦЕЛОЧИСЛЕННОЕ ЗНАЧЕНИЕ\n");   
+    Console.ResetColor();
+    Console.Write("Введите второе число: ");
+}
+Console.Write("Введите третье число: ");
+/*Проверка ввода первого числа на int*/
+while (!int.TryParse(Console.ReadLine(), out numberThree))
+{
+    Console.ForegroundColor = ConsoleColor.Red;//ошибка красного цвета
+    Console.WriteLine("ОШИБКА! ВЫ ВВЕЛИ НЕ ЦЕЛОЧИСЛЕННОЕ ЗНАЧЕНИЕ\n");   
+    Console.ResetColor();
+    Console.Write("Введите третье число: ");
+}
+
+int maximum = MaxNumber(numberOne, numberTwo, numberThree);//Вводим параметры для функции
+
+/*Ответ с оформлением*/
+Console.WriteLine("\n==============================");
 Console.WriteLine($"Максимальное число: {maximum}");
+Console.WriteLine("==============================");
