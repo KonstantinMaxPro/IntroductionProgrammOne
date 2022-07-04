e6464e130fa089a691a294201b3192c71ab532bd
Konstantin Mosov <paradyx@outlook.com>
2022-07-05 00:21:53 +0800

    modify homework Task 02 (украсил)

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,5 +1,5 @@
-﻿int numOne;
-int numTwo;
+﻿int numberFirst;
+int numberSecond;
 
 void MaxMin(int arg1, int arg2)
 {
@@ -13,17 +13,42 @@ void MaxMin(int arg1, int arg2)
         maximum = arg2;
         minimum = arg1;
     }
-    Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine($"Большее число: {maximum}");
-    Console.ForegroundColor = ConsoleColor.Red;
-    Console.WriteLine($"Меньшее число: {minimum}");
+    Console.Write("Число ");
+    Console.ForegroundColor = ConsoleColor.Green;//Даем зеленый цвет большему числу
+    Console.Write(maximum);
+    Console.ResetColor();//сброс на стандартный цвет
+    Console.Write(" больше числа ");
+    Console.ForegroundColor = ConsoleColor.Red;//Даем красный цвет меньшему числу
+    Console.WriteLine(minimum);
+    Console.ResetColor();//сброс на стандартный цвет
+}
+//ВНЕШНИЙ ВИД ПРОГРАММЫ КОНСОЛИ
+Console.Clear();//Очищаем консоль
+/*ЗАГОЛОВОК (ДЛЯ КРАСОТЫ)*/
+Console.WriteLine("==============================");
+Console.WriteLine("ОТНОШЕНИЕ ДВУХ ЧИСЕЛ");//Название
+Console.WriteLine("==============================\n");
+/*КОНЕЦ ЗАГОЛОВКА*/
+Console.Write("Введите первое число: ");
+/*Проверка ввода первого числа на int*/
+while (!int.TryParse(Console.ReadLine(), out numberFirst))
+{
+    Console.ForegroundColor = ConsoleColor.Red;//ошибка красного цвета
+    Console.WriteLine("ОШИБКА! ВЫ ВВЕЛИ НЕ ЦЕЛОЧИСЛЕННОЕ ЗНАЧЕНИЕ\n");   
+    Console.ResetColor();
+    Console.Write("Введите первое число: ");
 }
 
-
-Console.WriteLine("Введите два числа");
-Console.Write("Первое число: ");
-numOne = Convert.ToInt32(Console.ReadLine());
-Console.Write("Второе число: ");
-numTwo = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine();
-MaxMin(numOne, numTwo);
+Console.Write("Введите второе число: ");
+/*Проверка ввода второго числа на int*/
+while (!int.TryParse(Console.ReadLine(), out numberSecond))
+{
+    Console.ForegroundColor = ConsoleColor.Red;//ошибка красного цвета
+    Console.WriteLine("ОШИБКА! ВЫ ВВЕЛИ НЕ ЦЕЛОЧИСЛЕННОЕ ЗНАЧЕНИЕ\n");   
+    Console.ResetColor();
+    Console.Write("Введите второе число: ");
+}
+/*Ответ с оформлением*/
+Console.WriteLine("\n==============================");
+MaxMin(numberFirst, numberSecond);
+Console.WriteLine("==============================");
