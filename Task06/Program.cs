2e8e6885d04a38c7da1e5d72ac14c1187c69a1e8
Konstantin Mosov <paradyx@outlook.com>
2022-07-05 00:53:35 +0800

    modify homework Task 06 (офрмление)

--- Task06/Program.cs
+++ Task06/Program.cs
@@ -1,15 +1,41 @@
 ﻿int number;
-void Even(int arg1){
-if(arg1 % 2 == 0){
-    Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine($"Число {arg1}: ЧЁТНОЕ");
+
+void Even(int arg1)//Функция проверки на чётность
+{
+    if(arg1 % 2 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("ЧЁТНОЕ");
+        Console.ResetColor();
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("НЕЧЁТНОЕ");
+        Console.ResetColor();
+    }
 }
-else{
-    Console.ForegroundColor = ConsoleColor.Red;
-    Console.WriteLine($"Число {arg1}: НЕЧЁТНОЕ");
-}
-}
+
+//ВНЕШНИЙ ВИД ПРОГРАММЫ КОНСОЛИ
+Console.Clear();//Очищаем консоль
+/*ЗАГОЛОВОК (ДЛЯ КРАСОТЫ)*/
+Console.WriteLine("==============================");
+Console.WriteLine("ПРОВЕРКА ЧИСЛА НА ЧЁТНОСТЬ");//Название
+Console.WriteLine("==============================\n");
+/*КОНЕЦ ЗАГОЛОВКА*/
+
 Console.Write("Введите число: ");
-number = Convert.ToInt32(Console.ReadLine()); 
-Console.WriteLine();
+/*Проверка ввода первого числа на int*/
+while (!int.TryParse(Console.ReadLine(), out number))
+{
+    Console.ForegroundColor = ConsoleColor.Red;//ошибка красного цвета
+    Console.WriteLine("ОШИБКА! ВЫ ВВЕЛИ НЕ ЦЕЛОЧИСЛЕННОЕ ЗНАЧЕНИЕ\n");   
+    Console.ResetColor();
+    Console.Write("Введите число: ");
+}
+/*Ответ с оформлением*/
+Console.WriteLine("\n==============================");
+Console.Write($"Число {number}: ");
 Even(number);
+Console.WriteLine("==============================");
+
